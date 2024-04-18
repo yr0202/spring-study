d5eed05f370a5db8693235f1cc98ac972f35dcb3
yr0202 <73926438+yr0202@users.noreply.github.com>
2024-04-18 15:31:24 +0900

    spring-intro

---
+++ start-spring/start-spring/src/test/java/webSpring/startspring/StartSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package webSpring.startspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
