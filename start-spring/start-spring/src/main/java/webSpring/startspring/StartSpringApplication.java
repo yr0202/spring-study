7e9d754c069f3720d3fa7be89d45e90c0790029f
yr0202 <73926438+yr0202@users.noreply.github.com>
2024-04-25 21:46:10 +0900

    gradle build

--- start-spring/start-spring/src/main/java/webSpring/startspring/StartSpringApplication.java
+++ start-spring/start-spring/src/main/java/webSpring/startspring/StartSpringApplication.java
@@ -1,9 +1,7 @@
 package webSpring.startspring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class StartSpringApplication {
 
 	public static void main(String[] args) {
